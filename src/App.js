33066c85b52b16915e0eb2bf632d98ac88647a78
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-03-22 16:29:06 +0530

    Simplified app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import About from "./components/pages/About";
-import Academics from "./components/pages/Academics";
-import Campus from "./components/pages/Campus";
 import Contact from "./components/pages/Contact";
 import Forgot from "./components/pages/Forgot";
 import Home from "./components/pages/Home";
 import Myprofile from "./components/pages/Myprofile";
 import Signin from "./components/pages/Signin";
 import Signup from "./components/pages/Signup";
-// import NavBar from "./components/NavBar";
-import NavBar from "./components/HomeComponents/NavBar/NavBar.js";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -23,12 +20,6 @@ function App() {
         <Switch>
           <Route path="/about">
             <About />
-          </Route>
-          <Route path="/academics">
-            <Academics />
-          </Route>
-          <Route path="/campus">
-            <Campus />
           </Route>
           <Route path="/contact">
             <Contact />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,6 @@
 import React from "react";
-import Intro from "../HomeComponents/Intro";
+import Intro from "../Intro";
 import Footer from "../Footer";
-import HomeComponents from "../HomeComponents";
-import Advantages from "../HomeComponents/Advantages/Advantages";
-import Courses from "../HomeComponents/Courses/Courses";
-import Why from "../HomeComponents/Why/Why";
-import Companies from "../HomeComponents/Companies/Companies";
-import CampusLis from "../HomeComponents/Campus/CampusLis";
-import Facilities from "../HomeComponents/Facilities/Facilities";
-import Experience from "../HomeComponents/Experience/Experience";
 function Home() {
   return (
     <div className="pt-20 sm:pt-0">
@@ -16,13 +8,6 @@ function Home() {
       <div className="text-center">
         <i className="fa-solid fa-angles-down text-white absolute bottom-2 text-2xl md:text-3xl xl:text-4xl animate-bounce"></i>
       </div>
-      <Advantages />
-      <Why />
-      <Courses />
-      <Companies />
-      <CampusLis />
-      <Facilities />
-      <Experience />
       <Footer />
     </div>
   );
