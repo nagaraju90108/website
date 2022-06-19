57002bf67b0d511a49d1af318e6cdcec48ed7cfe
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-06-19 19:36:57 +0530

    Advantage comp in home is activated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,18 @@
-import React from "react";
-import Intro from "../Intro";
-import Footer from "../Footer";
+import React from 'react';
+import Intro from '../Intro';
+import Advantages from '../HomeComponents/Advantages/Advantages';
+import Footer from '../Footer';
 function Home() {
-  return (
-    <div className="pt-20 sm:pt-0">
-      <Intro />
-      <div className="text-center">
-        <i className="fa-solid fa-angles-down text-white absolute bottom-2 text-2xl md:text-3xl xl:text-4xl animate-bounce"></i>
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="pt-20 sm:pt-0">
+            <Intro />
+            <div className="text-center">
+                <i className="fa-solid fa-angles-down text-white absolute bottom-2 text-2xl md:text-3xl xl:text-4xl animate-bounce"></i>
+            </div>
+            <Advantages />
+            <Footer />
+        </div>
+    );
 }
 
 export default Home;
