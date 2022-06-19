1b4c4c66f28ffe431190307310e87951bc9d863b
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-06-19 19:49:59 +0530

    Advantages added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
-import Intro from "../Intro";
-import Footer from "../Footer";
+import React from 'react';
+import Intro from '../Intro';
+import Footer from '../Footer';
+import Advantages from '../HomeComponents/Advantages/Advantages';
 function Home() {
   return (
     <div className="pt-20 sm:pt-0">
@@ -8,6 +9,7 @@ function Home() {
       <div className="text-center">
         <i className="fa-solid fa-angles-down text-white absolute bottom-2 text-2xl md:text-3xl xl:text-4xl animate-bounce"></i>
       </div>
+      <Advantages />
       <Footer />
     </div>
   );
