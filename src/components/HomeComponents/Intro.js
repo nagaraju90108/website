4004b57dba4706f2f844fa89b5fc40bf5e15dbe7
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-06-19 22:39:55 +0530

    intro minor changes

--- src/components/HomeComponents/Intro.js
+++ src/components/HomeComponents/Intro.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Intro() {
-  const LoginStatus = localStorage.getItem("LoginStatus");
+  const LoginStatus = localStorage.getItem('LoginStatus');
   return (
     <>
       <div className=" min-h-[95vh] lg:min-h-[90vh] text-white flex flex-col items-center justify-center text-center">
         <div className="w-[90%] max-w-sm xs:max-w-md sm:max-w-xl md:max-w-3xl lg:max-w-[80%] xs:w-[80%] 2xl:w-10/12 px-0">
           <div>
-            <h1 className="font-bold text-3xl md:text-3xl lg:text-4xl">
-              ADVANCE YOUR CAREER WITH <br className="" />{" "}
+            <h1 className="font-bold text-3xl lg:text-4xl">
+              ADVANCE YOUR CAREER WITH <br className="" />{' '}
               <span className="text-mycolor text-4xl md:text-6xl lg:text-7xl sm:tracking-widest">
                 OSNR UNIVERSITY
               </span>
             </h1>
             <div className="introText xl:px-0 my-5 text-sm xl:text-base xl:my-6 text-justify">
-              One of India’s Best Private Universities, a premier Institute
+              One of India's Best Private Universities, a premier Institute
               providing the most enriching quality learning experience through
               cutting-edge research, innovative entrepreneurship, global
               exposure and the most lucrative career opportunities.
               <section className="hidden sm:inline">
                 <section className="hidden lg:inline">
                   Ranked among the top 10% of Universities in India, OSNR
-                  University is NAAC “A” Accredited and has received numerous
-                  National and International Awards. It has over 58 MOU’s with
+                  University is NAAC "A" Accredited and has received numerous
+                  National and International Awards. It has over 58 MOUs with
                   excellent infrastructure and an environment for
                   research-driven learning.
                 </section>
@@ -33,31 +33,31 @@ function Intro() {
 
           <div className="my-12 mx-auto max-w-sm flex flex-col space-y-6 sm:hidden">
             <Link
-              to={LoginStatus === "0" ? "/signin" : "/about"}
+              to={LoginStatus === '0' ? '/signin' : '/about'}
               className="mybtn_outline"
             >
-              {LoginStatus === "0" ? "EXPLORE" : "ABOUT US"}
+              {LoginStatus === '0' ? 'EXPLORE' : 'ABOUT US'}
             </Link>
             <Link
-              to={LoginStatus === "0" ? "/signup" : "/contact"}
+              to={LoginStatus === '0' ? '/signup' : '/contact'}
               className="mybtn"
             >
-              {LoginStatus === "0" ? "REGISTER" : "CONTACT US"}
+              {LoginStatus === '0' ? 'REGISTER' : 'CONTACT US'}
             </Link>
           </div>
 
           <div className="my-8 xl:my-10 hidden sm:block space-x-20 mx-auto">
             <Link
-              to={LoginStatus === "0" ? "/signin" : "/about"}
+              to={LoginStatus === '0' ? '/signin' : '/about'}
               className="mybtn_outline"
             >
-              {LoginStatus === "0" ? "EXPLORE" : "ABOUT US"}
+              {LoginStatus === '0' ? 'EXPLORE' : 'ABOUT US'}
             </Link>
             <Link
-              to={LoginStatus === "0" ? "/signup" : "/contact"}
+              to={LoginStatus === '0' ? '/signup' : '/contact'}
               className="mybtn"
             >
-              {LoginStatus === "0" ? "REGISTER" : "CONTACT US"}
+              {LoginStatus === '0' ? 'REGISTER' : 'CONTACT US'}
             </Link>
           </div>
         </div>
